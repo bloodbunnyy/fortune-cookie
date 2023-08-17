1d972b50b46500604510f1ff42b17551ba2d28b2
Jess <jnryan7@hotmail.com>
2023-08-17 16:02:54 +1000

    Changed program to output time now instead of fortune

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 // Create a basic "Hello World" server program
 
-const express = require('express');
-const child_process = require('child_process');
+//const express = require('express');
+//const child_process = require('child_process');
+
+
 // Create a new Express web application
-const app = express();
+
+//const app = express();
+
+
 // Set handler for the index of the website
-app.get('/', (req, res) => {
+
+//app.get('/', (req, res) => {
+
+
 // Run the system `fortune` command and respond with the message
-child_process.exec('fortune', (error, message) => {
-if(error === null) {
-res.send(message);
-} else {
-res.send('Error: ' + error);
-}
-})
+
+//child_process.exec('fortune', (error, message) => {
+//if(error === null) {
+//res.send(message);
+//} else {
+//res.send('Error: ' + error);
+//}
+//})
+//});
+const express = require('express');
+const app = express();
+const d = new Date();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!' + d);
 });
+
 // Start web application server
 app.listen(3000, () => {
 console.log('Server started');
